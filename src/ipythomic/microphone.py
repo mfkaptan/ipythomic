e75efb68d34f7012f0edca7324fae69250953462
mfkaptan <kaptanmf@gmail.com>
2015-02-25 02:36:21 +0200

    Add MicrophoneWidget Basic IPython html widget

--- src/ipythomic/microphone.py
+++ src/ipythomic/microphone.py
@@ -20,26 +20,31 @@ except Exception as exp:
 class MicrophoneWidget(DOMWidget):
     _view_name = Unicode("MicrophoneWidget", sync=True)
     value = Int(sync=True)
+    thickness = Int(sync=True)
+    background = Int(sync=True)
 
-    def __init__(self, **kwargs):
-        super(MicrophoneWidget, self).__init__(**kwargs)
-        self.inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NONBLOCK)
+    def __init__(self, thickness=5, background=250):
+        super(MicrophoneWidget, self).__init__()
+        self.thickness = thickness
+        self.background = background
+
+        self._in = alsaaudio.PCM(alsaaudio.PCM_CAPTURE, alsaaudio.PCM_NONBLOCK)
         # Set attributes: Mono, 8000 Hz, 16 bit little endian samples
-        self.inp.setchannels(1)
-        self.inp.setrate(8000)
-        self.inp.setformat(alsaaudio.PCM_FORMAT_S16_LE)
-        self.inp.setperiodsize(160)
+        self._in.setchannels(1)
+        self._in.setrate(8000)
+        self._in.setformat(alsaaudio.PCM_FORMAT_S16_LE)
+        self._in.setperiodsize(160)
 
         # Copy necessary javascript files
         self._prepare_js()
 
         # For calling a function continuously add it to ioloop callback list
-        self.ioloop = tornado.ioloop.IOLoop.current()  # IPython's
-        self.ioloop.add_callback(self.get_level)
+        self._ioloop = tornado.ioloop.IOLoop.current()  # IPython's
+        self._ioloop.add_callback(self.get_level)
 
     def get_level(self):
         # Read data from device
-        l, data = self.inp.read()
+        l, data = self._in.read()
         if l:
             # Return the maximum of the absolute value of
             # all samples in a fragment.
@@ -47,14 +52,13 @@ class MicrophoneWidget(DOMWidget):
             self.value = audioop.max(data, 2) / 50
 
         # Call this function again
-        self.ioloop.add_callback(self.get_level)
+        self._ioloop.add_callback(self.get_level)
 
     def _prepare_js(self):
         pkgdir = op.dirname(__file__)
         jsdir = op.join(pkgdir, '../js')
         install_nbextension(op.join(jsdir, 'processing.min.js'))
-        install_nbextension(op.join(jsdir, 'ipythomic.js'))
-        micvis_path = op.join(jsdir, 'micvis.js')
-        with open(micvis_path, 'r') as f:
+        ipythomic_js = op.join(jsdir, 'ipythomic.js')
+        with open(ipythomic_js, 'r') as f:
             script = f.read()
         display(Javascript(script))
